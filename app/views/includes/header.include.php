118b715b4b485fe37242a1770507a513ad07f652
Askas Jeremy <34890211+askasjeremy@users.noreply.github.com>
2024-03-01 22:02:09 +0300

    .

--- app/views/includes/header.include.php
+++ app/views/includes/header.include.php
@@ -1,19 +1,16 @@
 
 <header id="header" hx-preserve="true">
-  <!-- <menu>
-    <a href="<?=ROOT?>">Home</a>
-    <a href="<?=ROOT?>/products">Products</a>
-    <a href="<?=ROOT?>/login">Account</a>
-  </menu> -->
-
+  
   <div class="main_menu">
 
     <div class="wrapper">
       <?php
+        // one_menu_item() function is in the "functions.php" file inside the code folder
         one_menu_item('home','home','','Home','');
         one_menu_item('about','about','','About','');
         one_menu_item('login','login','','Login','');
       ?>
     </div>
   </div>
+  
 </header>
